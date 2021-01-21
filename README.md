a9810de5bf576eb80041814df92d7e318bbecc87
Johnson Zhong <zhsh@umich.edu>
2021-01-21 13:45:24 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# PyTorch Robot Kinematics
+- Parallel forward and inverse kinematics (FK and IK)
+- (TODO test this) Differentiable FK
+- Load robot description from URDF, SDF, and MJCF file 
+
+# Usage
+Clone repository somewhere, then `pip3 install -e .` to install in editable mode.
+
---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+setup(
+    name='pytorch_kinematics',
+    version='0.1.0',
+    packages=['pytorch_kinematics'],
+    url='https://github.com/UM-ARM-Lab/pytorch_kinematics',
+    license='MIT',
+    author='zhsh',
+    author_email='zhsh@umich.edu',
+    description='Robot kinematics implemented in pytorch',
+    install_requires=[
+        'torch',
+        'numpy'
+    ],
+    tests_require=[
+    ]
+)
