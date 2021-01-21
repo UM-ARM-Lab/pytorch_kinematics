a07ef3e65c41d9471ae514eb154d39d266afb5ef
Johnson Zhong <zhsh@umich.edu>
2021-01-21 14:26:31 -0500

    Include pytorch3d.transform in project

--- README.md
+++ README.md
@@ -6,3 +6,6 @@
 # Usage
 Clone repository somewhere, then `pip3 install -e .` to install in editable mode.
 
+# Transform Library
+The `pytorch_kinematics/transforms` is extracted from `pytorch3d` with only minor edits.
+This was done instead of including `pytorch3d` as a dependency because it is hard to install and most of its code is unrelated.
---
+++ pytorch_kinematics/transforms/__init__.py
@@ -0,0 +1,30 @@
+# Copyright (c) Facebook, Inc. and its affiliates. All rights reserved.
+
+from .rotation_conversions import (
+    axis_angle_to_matrix,
+    axis_angle_to_quaternion,
+    euler_angles_to_matrix,
+    matrix_to_euler_angles,
+    matrix_to_quaternion,
+    matrix_to_rotation_6d,
+    quaternion_apply,
+    quaternion_invert,
+    quaternion_multiply,
+    quaternion_raw_multiply,
+    quaternion_to_matrix,
+    random_quaternions,
+    random_rotation,
+    random_rotations,
+    rotation_6d_to_matrix,
+    standardize_quaternion,
+)
+from .so3 import (
+    so3_exponential_map,
+    so3_log_map,
+    so3_relative_angle,
+    so3_rotation_angle,
+)
+from .transform3d import Rotate, RotateAxisAngle, Scale, Transform3d, Translate
+
+
+__all__ = [k for k in globals().keys() if not k.startswith("_")]
