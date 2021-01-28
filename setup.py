2aa2aed503cc3cdd4bc717d1737089c530b188e7
Johnson Zhong <zhsh@umich.edu>
2021-01-28 17:45:57 -0500

    Mark for release 0.2.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='pytorch_kinematics',
-    version='0.1.0',
+    version='0.2.0',
     packages=['pytorch_kinematics'],
     url='https://github.com/UM-ARM-Lab/pytorch_kinematics',
     license='MIT',
