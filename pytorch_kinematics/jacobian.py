fix rotation error

pytorch_kinematics/jacobian.py
@@ -42,8 +42,8 @@ def calc_jacobian(serial_chain, th, tool=None):
                              -cur_transform[:, 0, 2] * cur_transform[:, 1, 3]
                              + cur_transform[:, 1, 2] * cur_transform[:, 0, 3]]).transpose(0, 1)
             # rows are the joint axis in the flange frame
-            axis_in_flange = cur_transform[:, :3, :3] @ f.joint.axis 
-            joint_to_eef_in_flange = -cur_transform[:, :3, :3].transpose(1,2) @ cur_transform[:, :3, 3].T
+            axis_in_flange = cur_transform[:, :3, :3].transpose(1, 2) @ f.joint.axis 
+            joint_to_eef_in_flange = cur_transform[:, :3, :3].transpose(1,2) @ cur_transform[:, :3, 3].T
             d = torch.cross(axis_in_flange, joint_to_eef_in_flange.squeeze(2), dim=1)
             delta = axis_in_flange #cur_transform[:, 2, 0:3]
             j_fl[:, :, -cnt] = torch.cat((d, delta), dim=-1)