e0da8959d1c70a908bcc4a7d7cb56ffbecc0f33e
Sheng Zhong <zhsh@umich.edu>
2023-10-18 18:20:53 -0400

    Implement Jacobian pseudo-inverse IK

---
+++ src/pytorch_kinematics/ik.py
@@ -0,0 +1,181 @@
+from pytorch_kinematics.chain import SerialChain
+from pytorch_kinematics.transforms import Transform3d
+from pytorch_kinematics.transforms import rotation_conversions
+from typing import NamedTuple, Union, List, Optional, Callable
+import torch
+from matplotlib import pyplot as plt, cm as cm
+
+
+class IKSolution(NamedTuple):
+    # N is the total number of attempts
+    err_pos: torch.Tensor
+    err_rot: torch.Tensor
+    # N boolean values indicating whether the solution converged (a solution could be found)
+    converged_pos: torch.Tensor
+    converged_rot: torch.Tensor
+    # whether both position and rotation converged
+    converged: torch.Tensor
+    # N x DOF tensor of joint angles; if converged[i] is False, then solutions[i] is undefined
+    solutions: torch.Tensor
+
+
+# helper config sampling method
+def gaussian_around_config(config: torch.Tensor, std: float) -> Callable[[int], torch.Tensor]:
+    def config_sampling_method(num_configs):
+        return torch.randn(num_configs, config.shape[0], dtype=config.dtype, device=config.device) * std + config
+
+    return config_sampling_method
+
+
+class InverseKinematics:
+    """Jacobian follower based inverse kinematics solver"""
+
+    def __init__(self, serial_chain: SerialChain,
+                 pos_tolerance: float = 1e-3, rot_tolerance: float = 1e-2,
+                 initial_configs: Optional[torch.Tensor] = None, num_retries: Optional[int] = None,
+                 joint_limits: Optional[torch.Tensor] = None,
+                 config_sampling_method: Union[str, Callable[[int], torch.Tensor]] = "uniform",
+                 max_iterations: int = 100, lr: float = 0.5,
+                 debug=True,
+                 ):
+        """
+        :param serial_chain:
+        :param pos_tolerance:
+        :param rot_tolerance:
+        :param initial_configs:
+        :param num_retries:
+        :param joint_limits: (DOF, 2) tensor of joint limits (min, max) for each joint in radians
+        :param config_sampling_method:
+        """
+        self.chain = serial_chain
+        self.dtype = serial_chain.dtype
+        self.device = serial_chain.device
+        joint_names = self.chain.get_joint_parameter_names(exclude_fixed=True)
+        self.dof = len(joint_names)
+        self.debug = debug
+
+        self.max_iterations = max_iterations
+        self.lr = lr
+
+        self.pos_tolerance = pos_tolerance
+        self.rot_tolerance = rot_tolerance
+        self.initial_config = initial_configs
+        if initial_configs is None and num_retries is None:
+            raise ValueError("either initial_configs or num_retries must be specified")
+
+        # sample initial configs instead
+        self.config_sampling_method = config_sampling_method
+        self.joint_limits = joint_limits
+        if initial_configs is None:
+            self.initial_config = self.sample_configs(num_retries)
+        else:
+            if initial_configs.shape[1] != self.dof:
+                raise ValueError("initial_configs must have shape (N, %d)" % self.dof)
+
+    def sample_configs(self, num_configs: int) -> torch.Tensor:
+        if self.config_sampling_method == "uniform":
+            # bound by joint_limits
+            if self.joint_limits is None:
+                raise ValueError("joint_limits must be specified if config_sampling_method is uniform")
+            return torch.rand(num_configs, self.dof, device=self.device) * (
+                    self.joint_limits[:, 1] - self.joint_limits[:, 0]) + self.joint_limits[:, 0]
+        elif self.config_sampling_method == "gaussian":
+            return torch.randn(num_configs, self.dof, device=self.device)
+        elif callable(self.config_sampling_method):
+            return self.config_sampling_method(num_configs)
+        else:
+            raise ValueError("invalid config_sampling_method %s" % self.config_sampling_method)
+
+    def solve(self, target_poses: Transform3d) -> IKSolution:
+        """
+        :param target_poses: (N, 4, 4) tensor
+        :return: (N, DOF) tensor of joint angles
+        """
+        raise NotImplementedError()
+
+
+class PseudoInverseIK(InverseKinematics):
+    def solve(self, target_poses: Transform3d) -> IKSolution:
+        target = target_poses.get_matrix()
+
+        target_pos = target[:, :3, 3]
+        # jacobian gives angular rotation about x,y,z axis of the base frame
+        # convert target rot to desired rotation about x,y,z
+        target_rot_rpy = rotation_conversions.matrix_to_euler_angles(target[:, :3, :3], "XYZ")
+
+        q = self.initial_config
+        # for logging, let's keep track of the joint angles at each iteration
+        if self.debug:
+            qs = [q]
+            pos_errors = []
+            rot_errors = []
+        for i in range(self.max_iterations):
+            # TODO early termination when below tolerance
+            # compute forward kinematics
+            fk = self.chain.forward_kinematics(q)
+            # N x 6 x DOF
+            J = self.chain.jacobian(q)
+
+            # compute pose difference
+            m = fk.get_matrix()
+            pos_diff = target_pos - m[:, :3, 3]
+            pos_diff = pos_diff.view(-1, 3, 1)
+            rot_diff = target_rot_rpy - rotation_conversions.matrix_to_euler_angles(m[:, :3, :3], "XYZ")
+            rot_diff = rot_diff.view(-1, 3, 1)
+            # pose_diff = target - fk.get_matrix()
+            J_pos = J[:, :3, :]
+            J_pos_pinv = torch.pinverse(J_pos)
+            J_rot = J[:, 3:, :]
+            J_rot_pinv = torch.pinverse(J_rot)
+            # compute joint angle difference
+            dq_pos = J_pos_pinv @ pos_diff
+            dq_rot = J_rot_pinv @ rot_diff
+
+            dq = dq_pos.squeeze(2) + dq_rot.squeeze(2)
+            q = q + self.lr * dq
+            if self.debug:
+                qs.append(q)
+                pos_errors.append(pos_diff.reshape(-1, 3).norm(dim=1))
+                rot_errors.append(rot_diff.norm(dim=(1, 2)))
+
+        if self.debug:
+            # qs = torch.stack(qs, dim=0)
+            # fig, ax = plt.subplots(ncols=self.dof, figsize=(10, 5))
+            # for dof in range(self.dof):
+            #     # don't show everyone, skip some
+            #     for b in range(0, qs.shape[1], 5):
+            #         c = (b + 1) / qs.shape[1]
+            #         ax[dof].plot(qs[:, b, dof], c=cm.GnBu(c))
+
+            # errors
+            fig, ax = plt.subplots(ncols=2, figsize=(10, 5))
+            pos_e = torch.stack(pos_errors, dim=0).cpu()
+            rot_e = torch.stack(rot_errors, dim=0).cpu()
+            ax[0].set_ylim(0, 1.)
+            ax[1].set_ylim(0, rot_e.max().item() * 1.1)
+            ax[0].set_xlim(0, self.max_iterations - 1)
+            ax[1].set_xlim(0, self.max_iterations - 1)
+            # draw at most 50 lines
+            draw_max = min(50, pos_e.shape[1])
+            for b in range(draw_max):
+                c = (b + 1) / draw_max
+                ax[0].plot(pos_e[:, b], c=cm.GnBu(c))
+                ax[1].plot(rot_e[:, b], c=cm.GnBu(c))
+            # label these axis
+            ax[0].set_ylabel("position error")
+            ax[1].set_xlabel("iteration")
+            ax[1].set_ylabel("rotation error")
+            plt.show()
+
+        # check convergence
+        # fk = self.chain.forward_kinematics(q)
+        # pose_diff = target - fk.get_matrix()
+        err_pos = pos_diff.squeeze(2).norm(dim=1)
+        err_rot = rot_diff.squeeze(2).norm(dim=1)
+
+        converged_pos = err_pos < self.pos_tolerance
+        converged_rot = err_rot < self.rot_tolerance
+
+        return IKSolution(converged_pos=converged_pos, converged_rot=converged_rot,
+                          err_pos=err_pos, err_rot=err_rot,
+                          converged=converged_pos & converged_rot, solutions=q)
---
+++ tests/test_inverse_kinematics.py
@@ -0,0 +1,98 @@
+import math
+import os
+from timeit import default_timer as timer
+
+import torch
+
+import pytorch_kinematics as pk
+
+import pybullet as p
+import pybullet_data
+
+TEST_DIR = os.path.dirname(__file__)
+
+visualize = True
+
+
+def _make_robot_translucent(robot_id, alpha=0.4):
+    def make_transparent(link):
+        link_id = link[1]
+        rgba = list(link[7])
+        rgba[3] = alpha
+        p.changeVisualShape(robot_id, link_id, rgbaColor=rgba)
+
+    visual_data = p.getVisualShapeData(robot_id)
+    for link in visual_data:
+        make_transparent(link)
+
+
+def test_jacobian_follower():
+    device = "cuda" if torch.cuda.is_available() else "cpu"
+    # device = "cpu"
+    urdf = "kuka_iiwa/model.urdf"
+    search_path = pybullet_data.getDataPath()
+    full_urdf = os.path.join(search_path, urdf)
+    chain = pk.build_serial_chain_from_urdf(open(full_urdf).read(), "lbr_iiwa_link_7")
+    chain = chain.to(device=device)
+
+    joints_high = torch.tensor([170, 120, 170, 120, 170, 120, 175], device=device)
+    joint_limits = torch.stack((-joints_high, joints_high), dim=-1) * math.pi / 180.0
+    ik = pk.PseudoInverseIK(chain, max_iterations=50, num_retries=10000, joint_limits=joint_limits)
+
+    # robot frame
+    pos = torch.tensor([0.0, 0.0, 0.0], device=device)
+    rot = torch.tensor([0.0, 0.0, 0.0], device=device)
+    rob_tf = pk.Transform3d(pos=pos, rot=rot, device=device)
+
+    # world frame goal
+    goal_pos = torch.tensor([0.0, 0.5, 0.5], device=device)
+    goal_rot = torch.tensor([0.0, 0.0, 0.0], device=device)
+    goal_tf = pk.Transform3d(pos=goal_pos, rot=goal_rot, device=device)
+
+    # transform to robot frame
+    goal_in_rob_frame_tf = rob_tf.inverse().compose(goal_tf)
+
+    # do IK
+    timer_start = timer()
+    sol = ik.solve(goal_in_rob_frame_tf)
+    timer_end = timer()
+    print("IK took %f seconds" % (timer_end - timer_start))
+    print("IK converged number: %d / %d" % (sol.converged.sum(), sol.converged.shape[0]))
+
+    # visualize everything
+    if visualize:
+        p.connect(p.GUI)
+        p.setRealTimeSimulation(False)
+        p.configureDebugVisualizer(p.COV_ENABLE_GUI, 0)
+        p.setAdditionalSearchPath(search_path)
+        p.loadURDF("plane.urdf", [0, 0, 0], useFixedBase=True)
+        m = rob_tf.get_matrix()
+        pos = m[0, :3, 3]
+        rot = m[0, :3, :3]
+        quat = pk.matrix_to_quaternion(rot)
+        armId = p.loadURDF(urdf, basePosition=pos.cpu().numpy(), baseOrientation=pk.wxyz_to_xyzw(quat).cpu().numpy(),
+                           useFixedBase=True)
+        _make_robot_translucent(armId, alpha=0.6)
+        # p.resetBasePositionAndOrientation(armId, [0, 0, 0], [0, 0, 0, 1])
+        # draw goal
+        # place a translucent sphere at the goal
+        visId = p.createVisualShape(p.GEOM_SPHERE, radius=0.05, rgbaColor=[0., 1., 0., 0.5])
+        goalId = p.createMultiBody(baseMass=0, baseVisualShapeIndex=visId, basePosition=goal_pos.cpu().numpy())
+
+        yaw = 90
+        pitch = -55
+        dist = 1.
+        target = goal_pos.cpu().numpy()
+        p.resetDebugVisualizerCamera(dist, yaw, pitch, target)
+
+        for i, q in enumerate(sol.solutions):
+            input("Press enter to continue")
+            for dof in range(q.shape[0]):
+                p.resetJointState(armId, dof, q[dof])
+
+        while True:
+            p.stepSimulation()
+
+
+if __name__ == "__main__":
+    test_jacobian_follower()
