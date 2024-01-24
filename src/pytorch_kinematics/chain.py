fix jacobian calc

src/pytorch_kinematics/chain.py
@@ -506,7 +506,6 @@ class Chain:
         # retrieve desired link-transform
         ee_transform = T[link_indices, N_range] @ cur_transform
         tool_world = ee_transform[:, :3, 3]
-
         # compute jacobian in world frame
         jacobian = torch.zeros((N, 6, ndof), dtype=self.dtype, device=self.device)
         # TODO exclude fixed joints? saves for loop time
@@ -515,6 +514,9 @@ class Chain:
         for d in range(self.max_kinematic_tree_depth):
             # Retrieve frame information
             frame_idx = self.non_fixed_parents_indices[link_indices, d]
+            # break if have calculated for all desired links
+            if torch.all(frame_idx < 0):
+                break
             transform = T[frame_idx, N_range]
             joint_idx = self.joint_indices[frame_idx]
             joint_axes = self.axes[joint_idx].expand(N, 3).unsqueeze(-1)
@@ -548,9 +550,7 @@ class Chain:
                                                           jacobian_col,
                                                           old_jacobian_col)
 
-            # break if have calculated for all desired links
-            if torch.all(frame_idx < 0):
-                break
+
 
         return jacobian
 
@@ -613,7 +613,6 @@ class SerialChain(Chain):
         self._serial_frames = [self._root] + self._generate_serial_chain_recurse(self._root, end_frame_name)
         if self._serial_frames is None:
             raise ValueError("Invalid end frame name %s." % end_frame_name)
-        # self.convert_serial_inputs_to_chain_inputs = torch.vmap(self._convert_serial_inputs_to_chain_inputs, in_dims=(None, 1))
 
     def jacobian(self, th, locations=None, link_indices=None):
         if locations is not None:
@@ -651,6 +650,7 @@ class SerialChain(Chain):
             th = torch.tensor(th, dtype=self.dtype, device=self.device)
         if len(th.shape) <= 1:
             th = th.reshape(1, -1)
+            N = 1
         else:
             N = th.shape[0]
         th_size = get_th_size(th)